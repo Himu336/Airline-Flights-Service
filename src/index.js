a7375344078139bd06aff0c283c259b4f9eb79e2
Himansh <himanshbansal200521@gmail.com>
2025-09-08 14:18:59 +0530

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { serverConfig, logger } = require("./config");
 const express = require("express");
+const cors = require('cors');
 const apiRoutes = require("./routes");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
